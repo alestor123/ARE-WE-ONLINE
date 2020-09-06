5ebaeb414489738cfccc20b992001c53da15271d
alestor123 <alestoraldous@gmail.com>
2020-09-06 20:01:39 +0530

    added main code

--- run.js
+++ run.js
@@ -1,1 +1,9 @@
 var  areweon = require('./App')
+areweon(function (online) {
+    if (online) {
+      console.log('connected to the internet hurray !')
+    } else {
+      console.error('sorry, not connected! get some help')
+    }
+  })
+  
