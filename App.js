74569b6e52f9085017cf6f5e8c4ac8c9c30a1257
alestor123 <alestoraldous@gmail.com>
2020-09-06 19:58:40 +0530

    val error

--- App.js
+++ App.js
@@ -6,4 +6,19 @@ module.exports = function (cb) {
         port: 80,
         host: 'nodejs.org'
       })
+  // If no 'error' or 'connect' event after 5s, assume network is down
+  var timer = setTimeout(function () {
+    done(new Error('timeout'))
+  }, 5000)
+
+  var done = once(function (err) {
+    clearTimeout(timer)
+    socket.unref()
+    socket.end()
+    cb(!err) // eslint-disable-line standard/no-callback-literal
+  })
+
+  socket.on('error', done)
+  socket.on('connect', done)
 }
+    }
